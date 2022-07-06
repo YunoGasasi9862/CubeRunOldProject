dd53e8de895385d0f9c2c541eec077f3a298d13c
Muhammad Bilal <m.bilal9862@gmail.com>
2022-07-06 13:29:19 +0300

    ss

---
+++ Assets/GroundGenerator.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float minDistance = 0;
+    public GameObject plane;
+    void Start()
+    {
+        StartCoroutine(SpawnGround());
+    }
+
+  
+   IEnumerator SpawnGround()
+    {
+
+        while(true)
+        {
+             
+            Vector3 position= transform.position;
+            
+            position.z = minDistance;
+            GameObject ground = Instantiate(plane, position, plane.transform.rotation);
+            minDistance += 15;
+            yield return null;
+        
+
+        }
+    }
+}
