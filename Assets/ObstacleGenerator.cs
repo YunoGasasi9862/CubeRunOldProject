ffa46e036c3513fc851751ea2ef5a13a7c8b5099
Muhammad Bilal <m.bilal9862@gmail.com>
2022-07-12 12:41:30 +0300

    ss

--- Assets/ObstacleGenerator.cs
+++ Assets/ObstacleGenerator.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class ObstacleGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    int numberofobstacles = 150;
+    float minz=20.0f, maxz=970f;
+    float minX=-5.7f, maxX=3.2f;
+    public GameObject obstacle;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartCoroutine(SpawnObstacles());
+    }
+    IEnumerator SpawnObstacles()
+    {
+
+        Vector3 pos;
+        for (int i = 0; i < numberofobstacles; i++)
+        {
+            pos.x = Random.Range(minX, maxX);
+            pos.z = Random.Range(minz, maxz);
+            GameObject obstaclebody = Instantiate(obstacle, pos, Quaternion.identity); 
+
+
+        }
+        yield return null;
     }
 }
